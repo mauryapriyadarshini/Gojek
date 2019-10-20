986ed524d22095c856b15561ddd4b1e3771a511b
Priyadarshini Maurya <pmaurya@quotient.com>
2019-10-20 19:34:12 +0530

    Gojeck: Implement Ui Skeleton - Added all image assets. - Added all layout files. - Added styles for text. - Added menu in Home Screen.

--- app/src/main/java/com/example/gojeck/GoJeckBaseActivity.kt
+++ app/src/main/java/com/example/gojeck/GoJeckBaseActivity.kt
@@ -2,11 +2,32 @@ package com.example.gojeck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+
 
 class GoJeckBaseActivity : AppCompatActivity() {
+
+    private var toolbar: Toolbar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.gojeck_base_activity)
+
+        toolbar = findViewById(R.id.trToolbar)
+        setSupportActionBar(toolbar)
+    }
+
+   // <---------- ToolBar function ------>
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.getItemId()
+        return super.onOptionsItemSelected(item)
     }
 }
